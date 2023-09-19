6f4546fa4968f62ffcde850c610e9af6f993ec55
nova0035 <padaliyabhavya834@gmail.com>
2023-09-19 13:51:56 +0000

    LOGIN CODE FOR ADMIN DONE

--- Login.java
+++ Login.java
@@ -41,4 +41,45 @@ public class Login {
             return -2; // SQL exception occurred
         }
     }
+
+    public int adminLogin(int admin_id, String password) {
+        
+        try {
+            // Establish a database connection
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/krishna_medical_store", "root", "");
+
+            // Prepare an SQL query to retrieve staff information
+            String query = "SELECT * FROM admin_login WHERE admin_id = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, admin_id);
+
+            // Execute the SQL query and retrieve the result set
+            ResultSet admin_login_table = stmt.executeQuery();
+
+            // Check if the result set is empty (no matching admin_id)
+            if (!admin_login_table.next()) {
+                System.out.println("\nAdmin ID Not Found -");
+                return -1; // Admin ID not found in the database
+            }
+
+            else {
+                // Retrieve the stored password from the result set
+                String storedPassword = admin_login_table.getString("password");
+
+                // Compare the entered password with the stored password
+                if (password.equals(storedPassword)) {
+                    System.out.println("\nLogin Successful -");
+                    return 1; // Password matches, login successful
+                }
+                else {
+                    System.out.println("\nPassword Is Wrong -");
+                    return 0; // Incorrect password
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("\nSQLException Occurred: " + e.getMessage());
+            return -2; // SQL exception occurred
+        }
+    }
 }
