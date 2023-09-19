f46ced810930bccdc9a5637b299b93b5ccd75402
nova0035 <padaliyabhavya834@gmail.com>
2023-09-19 17:16:45 +0000

    ADD NEW MEDICINE CODE UPDATED WITH CHECK MEDICINE EXISTS OR NOT AND COMMENTS ADDED

--- Stock.java
+++ Stock.java
@@ -1,16 +1,20 @@
 import java.sql.*;
 
 public class Stock {
+    // Method to add a new medicine to the stock database
     void addNewMedicine(String manufacturerName, String medicineName, int power, int pricePerTablet, int quantity) {
         try {
             // Establish a database connection
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/krishna_medical_store", "root", "");
 
+            // Create a statement to execute SQL queries
             Statement stmt = conn.createStatement();
 
+            // Execute a SELECT query to check if the medicine already exists in the database
             ResultSet table = stmt.executeQuery("SELECT * FROM stock_data WHERE medicine_name = '" + medicineName + "';");
 
-            if(!table.next()){
+            // Check if the medicine is not found in the database
+            if (!table.next()) {
 
                 // Create a SQL prepared statement with parameter binding
                 String sql = "INSERT INTO stock_data (manufacturer_name, medicine_name, power, price_per_tablet, quantity) VALUES (?, ?, ?, ?, ?)";
@@ -21,23 +25,27 @@ public class Stock {
                 pstmt.setInt(4, pricePerTablet);
                 pstmt.setInt(5, quantity);
 
+                // Execute the INSERT query and get the number of rows affected
                 int rowsAffected = pstmt.executeUpdate();
 
+                // Check if the insertion was successful
                 if (rowsAffected == 1) {
                     System.out.println("\nNew Medicine Added Successfully");
                 } else {
                     System.out.println("\nMedicine Data Cannot Be Added");
                 }
 
+                // Close the prepared statement and database connection
                 pstmt.close();
                 conn.close();
 
-            }
-            else{
+            } else {
+                // If the medicine already exists in the database, display a message
                 System.out.println("\nMedicine Already Exists -");
             }
-            
+
         } catch (SQLException e) {
+            // Handle any SQL-related exceptions and display an error message
             System.out.println("\nSQLException Occurred: " + e.getMessage());
             e.printStackTrace();
         }
