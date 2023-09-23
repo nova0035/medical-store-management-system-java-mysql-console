00e657e2a3cf0fc60d133d0a0d32c4f5a507b64f
nova0035 <padaliyabhavya834@gmail.com>
2023-09-23 09:17:20 +0000

    SEARCH MEDICINE BY NAME AND POWER CODE DONE

--- Stock.java
+++ Stock.java
@@ -98,5 +98,80 @@ public class Stock {
             e.printStackTrace();
             System.out.println("\nSQLException Occurred -");
         }
-    }       
+    }  
+    // Method to search for medicine in the database by its name and power
+    void searchMedicine(String medicine_name, String power) {
+        try {
+            // Establish a database connection
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/krishna_medical_store", "root", "");
+        
+            // Create a statement to execute SQL queries
+            Statement stmt = conn.createStatement();
+            ResultSet medicineData;
+        
+            String manufacturer_name, med_name;
+            int pwr, price_per_tablet, quantity;
+        
+            // Check if power is "0" (assuming "0" means no power specified)
+            if ("0".equals(power)) {
+                // Search for medicine by name without considering power
+                medicineData = stmt.executeQuery("SELECT * FROM stock_data WHERE medicine_name = '" + medicine_name + "';");
+        
+                // Check if the medicine data was found
+                if (!medicineData.next()) {
+                    System.out.println("\nMedicine Data Not Found -");
+                } else {
+                    // Display a header for the table
+                    System.out.println("\nManufacturer Name   | Medicine Name     | Power       | Price Per Tablet | Quantity\n");
+        
+                    do {
+                        // Retrieve data from the ResultSet
+                        manufacturer_name = medicineData.getString(1);
+                        med_name = medicineData.getString(2);
+                        pwr = medicineData.getInt(3);
+                        price_per_tablet = medicineData.getInt(4);
+                        quantity = medicineData.getInt(5);
+        
+                        // Define the format for displaying medicine data as a table
+                        String format = "%-19s | %-17s | %-11d | %-16d | %-8d";
+        
+                        // Use String.format() to format and display the data
+                        System.out.println(String.format(format, manufacturer_name, med_name, pwr, price_per_tablet, quantity));
+        
+                    } while (medicineData.next());
+                }
+            } else {
+                // Search for medicine by both name and power
+                medicineData = stmt.executeQuery("SELECT * FROM stock_data WHERE medicine_name = '" + medicine_name + "' AND power = '" + power + "';");
+        
+                // Check if the medicine data was found
+                if (!medicineData.next()) {
+                    System.out.println("\nMedicine Data Not Found -");
+                } else {
+                    // Display a header for the table
+                    System.out.println("\nManufacturer Name   | Medicine Name     | Power       | Price Per Tablet | Quantity");
+        
+                    do {
+                        // Retrieve data from the ResultSet
+                        manufacturer_name = medicineData.getString(1);
+                        med_name = medicineData.getString(2);
+                        pwr = medicineData.getInt(3);
+                        price_per_tablet = medicineData.getInt(4);
+                        quantity = medicineData.getInt(5);
+        
+                        // Define the format for displaying medicine data as a table
+                        String format = "\n%-19s | %-17s | %-11d | %-16d | %-8d";
+        
+                        // Use String.format() to format and display the data
+                        System.out.println(String.format(format, manufacturer_name, med_name, pwr, price_per_tablet, quantity));
+        
+                    } while (medicineData.next());
+                }
+            }
+        } catch (SQLException e) {
+            // Handle any SQL exceptions
+            System.out.println("\nSQLException Occurred");
+        }
+    }
+     
 }
